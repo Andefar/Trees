55609e07b88385291a4fbb3104c5de20eb52ac0a
andefar40 <andreas.david.lauritzen@gmail.com>
2016-01-17 17:16:38 +0100

    moved draw in

--- TreeSolution/TreeProj/Draw.fsx
+++ TreeSolution/TreeProj/Draw.fsx
@@ -1,21 +1,71 @@
 ﻿open System.IO
+
+
+type Tree<'a> = Node of (string*float) * (Tree<'a> list)
+
 let path = @"/Users/AndreasLauritzen/tree.ps"
 let output = new System.Text.StringBuilder()
 
-let initPS() = output.Append "%!\n<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice\n1 1 scale\nnewpath\n/Times-Roman findfont 10 scalefont setfont\n"
+let initPS() = 
+    let start = ["%!\n";
+                 "<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice\n";
+                 "1 1 scale\n";
+                 "700 999 translate\n";
+                 "newpath\n";
+                 "/Times-Roman findfont 10 scalefont setfont\n"]
+    String.concat "" start
 
-let endPS() = output.Append "showpage"
+let endPS() = "showpage" 
 
-let toFile() = File.WriteAllText(path,output.ToString())
+let toFile(sb:System.Text.StringBuilder) = File.WriteAllText(path,sb.ToString())
 
-let label(s) =
-    let strings = [" (";s;") dup stringwidth pop 2 div neg 0 rmoveto show\n"] 
-    let fullString = String.concat ", " strings
-    output.Append fullString
+let stroke() = "stroke\n"
+
+let label(s,(x,y)) =
+    let strings = [x.ToString();" ";y.ToString();" moveto\n";
+                   " (";s;") dup stringwidth pop 2 div neg 0 rmoveto show\n"] 
+    String.concat "" strings
+
+let line ((xStart,yStart),(xEnd,yEnd)) = 
+    let strings = [xStart.ToString();" ";yStart.ToString();" moveto\n";
+                   xEnd.ToString();" ";yEnd.ToString();" lineto\n";] 
+    String.concat "" strings
+
+let rec drawList (x:float,y:float) subtree =
+    String.concat "" (List.map (fun tree -> drawSub (x,y) tree) subtree)
+
+and drawSub (x:float,y:float) = function
+    | Node((lab,offset),[])      -> let newX = (x+(offset*60.0))
+                                    String.concat "" [label(lab,(newX,y));line((newX,y+10.0),(newX,y+30.0));stroke()]
+
+    | Node((lab,offset),subtree) -> let newX = (x+(offset*60.0))
+                                    let newY = y-60.0
+                                    String.concat "" [label(lab,(newX,y));line((newX,y+10.0),(newX,y+30.0));line((newX,y-10.0),(newX,y-30.0));(drawList (newX,newY) subtree);stroke()]
 
 
 
-initPS();;
-label("hej");;
-endPS();;
-toFile();;
+
+let draw ((tree: Tree<string * float>),(sb:System.Text.StringBuilder)) = 
+                ignore(sb.Append (initPS()))
+                ignore(sb.Append (drawSub (0.0,-50.0) tree)) 
+                ignore(sb.Append (endPS()))
+                toFile(sb)
+
+
+
+(* TESTS *)
+let testTree = Node(("1", 0.0),
+                               [Node (("2", -1.0),
+                                                  [Node (("3", -0.5),[]);
+                                                   Node (("4", 0.5),[])
+                                                  ]);
+                                                   Node (("5", 0.0),[]);
+                                                   Node (("6", 1.0),
+                                                                    [Node (("7", -0.5),[]);
+                                                                     Node (("8", 0.5),[])
+                                                                    ]
+                                                         )
+                                ]
+                     )
+
+draw (testTree,output);;
