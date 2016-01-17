7f14aa81b2da5165fb7b315acda00baa3602b9bf
andefar40 <andreas.david.lauritzen@gmail.com>
2016-01-17 17:12:50 +0100

    drawing file updated

--- Draw.fsx
+++ Draw.fsx
@@ -1,4 +1,8 @@
 ﻿open System.IO
+
+
+type Tree<'a> = Node of (string*float) * (Tree<'a> list)
+
 let path = @"/Users/AndreasLauritzen/tree.ps"
 let output = new System.Text.StringBuilder()
 
@@ -9,30 +13,59 @@ let initPS() =
                  "700 999 translate\n";
                  "newpath\n";
                  "/Times-Roman findfont 10 scalefont setfont\n"]
-    output.Append (String.concat "" start)
+    String.concat "" start
 
-let endPS() = output.Append "showpage"
+let endPS() = "showpage" 
 
-let toFile() = File.WriteAllText(path,output.ToString())
+let toFile(sb:System.Text.StringBuilder) = File.WriteAllText(path,sb.ToString())
 
-
-let stroke() = output.Append "stroke\n"
+let stroke() = "stroke\n"
 
 let label(s,(x,y)) =
     let strings = [x.ToString();" ";y.ToString();" moveto\n";
                    " (";s;") dup stringwidth pop 2 div neg 0 rmoveto show\n"] 
-    output.Append (String.concat "" strings)
+    String.concat "" strings
 
 let line ((xStart,yStart),(xEnd,yEnd)) = 
     let strings = [xStart.ToString();" ";yStart.ToString();" moveto\n";
                    xEnd.ToString();" ";yEnd.ToString();" lineto\n";] 
-    output.Append (String.concat "" strings)
+    String.concat "" strings
 
- 
+let rec drawList (x:float,y:float) subtree =
+    String.concat "" (List.map (fun tree -> drawSub (x,y) tree) subtree)
+
+and drawSub (x:float,y:float) = function
+    | Node((lab,offset),[])      -> let newX = (x+(offset*60.0))
+                                    String.concat "" [label(lab,(newX,y));stroke()]
+
+    | Node((lab,offset),subtree) -> let newX = (x+(offset*60.0))
+                                    let newY = y-60.0
+                                    String.concat "" [label(lab,(newX,y));(drawList (newX,newY) subtree);stroke()]
+
+
+
+
+let draw ((tree: Tree<string * float>),(sb:System.Text.StringBuilder)) = 
+                ignore(sb.Append (initPS()))
+                ignore(sb.Append (drawSub (0.0,-50.0) tree)) 
+                ignore(sb.Append (endPS()))
+                toFile(sb)
+
+
+
 (* TESTS *)
-initPS();;
-label("hej",(0,-50));;
-line((0,-60),(50,-80))
-stroke();;
-endPS();;
-toFile();;
+let testTree = Node(("1", 0.0),
+                               [Node (("2", -1.0),
+                                                  [Node (("3", -0.5),[]);
+                                                   Node (("4", 0.5),[])
+                                                  ]);
+                                                   Node (("5", 0.0),[]);
+                                                   Node (("6", 1.0),
+                                                                    [Node (("7", -0.5),[]);
+                                                                     Node (("8", 0.5),[])
+                                                                    ]
+                                                         )
+                                ]
+                     )
+
+draw (testTree,output);;
